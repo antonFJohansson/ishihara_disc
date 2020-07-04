d39e050bb631a8c6198348892c568760337f4353
Anton Johansson <johaant@chalmers.se>
2020-07-04 18:14:35 +0200

    Fix: Fixed it so that it is now possible to add an object

--- mainFile.py
+++ mainFile.py
@@ -7,15 +7,25 @@ Created on Sat Jul  4 13:50:46 2020
 
 
 from ishiharaDiscFile import create_ishihara_disc, plot_ishihara_disc
+from objectContainmentFile import check_object_containment
 
 WIDTH = 1
 HEIGHT = 1
 PIXEL_SCALE = 1000
+num_circles = 720
 save_name = 'ishihara_disc.png'
+object_path = 'object_img_2.PNG'
 
-x_pos, y_pos, rad = create_ishihara_disc()
+x_pos, y_pos, rad = create_ishihara_disc(num_circles = num_circles)
+object_circles = check_object_containment(x_pos, y_pos, object_path)
+plot_ishihara_disc(x_pos, y_pos, rad, object_circles, save_name = save_name)
 
-plot_ishihara_disc(x_pos, y_pos, rad, save_name = save_name)
+#import matplotlib.pyplot as plt
+#plt.imshow(data)
+
+
+
+
 
 
 
