39cbc49ce910c7d6939ba68aee2de273c4b39412
antonFJohansson <47623953+antonFJohansson@users.noreply.github.com>
2020-07-06 22:52:22 +0200

    Update mainFile.py

--- mainFile.py
+++ mainFile.py
@@ -11,35 +11,33 @@ from objectContainmentFile import check_object_containment
 from displayObjectFile import manipulate_object
 from colourFile import convert_object_colour
 
-WIDTH = 1
-HEIGHT = 1
+
+## Final resolution of the image
 PIXEL_SCALE = 1000
+## Num circles in the given disc
 num_circles = 720
-save_name = 'test_light_2.png'
-object_path = 'shrek_2.png'
+
+## Save name of the final image
+save_name = 'final_ishihara_disc.png'
+## Path to the binary image with the object
+object_path = 'my_object.png'
+## The colour of the object (black/white)
 object_colour = 'black'
+## Colour scheme, the ones possible are
+##      red-green , dark-green-red and dark-blue-red
 colour_scheme = 'red-green'
+
+## Convert colour to binary digit
 object_colour = convert_object_colour(object_colour)
-
-
+## Create Ishihara disc
 x_pos, y_pos, rad = create_ishihara_disc(num_circles = num_circles)
+## Obtain binary object image
 object_img = manipulate_object(object_path, object_colour = object_colour)
+## Obtain which circles should represent the object
 object_circles = check_object_containment(x_pos, y_pos, object_img,
                                           object_colour = object_colour)
+## Plot and save the Ishihara_disc
 plot_ishihara_disc(x_pos, y_pos, rad, object_circles, colour_scheme, save_name = save_name)
-
-
-
-## And now in the last step we would maybe like to just see that our object will look nice when we load it
-## So maybe easiest to see that the image will be loaded in openCv into a np array or something
-
-## So tomorrow just add comments and function help
-## And then make it so that I can present it and put it on my website?
-## And maybe some more colour schemes
-
-
-#import matplotlib.pyplot as plt
-#plt.imshow(data)
 
 
 
